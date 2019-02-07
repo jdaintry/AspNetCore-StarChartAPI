f91160acc65eb6ce7ffe8106a5e8c13860a37e5b
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-07 18:55:56 +1100

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -22,7 +22,13 @@ namespace StarChart.Controllers
             var celestralObject = _context.CelestialObjects.Find(id);
             if (celestralObject == null)
                 return NotFound();
-            celestralObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id);
+            celestralObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+            return Ok(celestralObject);
         }
+         [HttpGet("{name}"})]
+          public IActionResult GetByName(string name)
+           {
+        var celestialObjects = _context.CelestialObjects.Where(e => e.Name == name).ToList();
+           }
     }
 }
