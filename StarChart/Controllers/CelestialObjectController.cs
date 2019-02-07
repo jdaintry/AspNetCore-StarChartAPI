d4ccaf623c38a1c2017effb171751e122837e9cc
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-07 20:14:08 +1100

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,11 +17,19 @@ namespace StarChart.Controllers
             _context = context;
         }
 
-        
-        
 
-
-
+        [HttpGet("{name}"})]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Name == name).ToList();
+            if (!celestialObjects.Any())
+            return NotFound();
+               foreach (var celestialObject in celestialObjects)
+               {
+               celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+               }
+           return Ok(celestialObjects);
+        }
 
     }
 }
